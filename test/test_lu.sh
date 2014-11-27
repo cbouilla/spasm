4a1fdcd195a91772ae7f849f47c743b58b62363a
Charles Bouillaguet <charles.bouillaguet@lifl.fr>
2014-11-27 10:09:53 +0100

    test with non-invertible matrix

--- test/test_lu.sh
+++ test/test_lu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo 1..2
+echo 1..3
 
 #########################################
 INPUT_MATRIX=$srcdir/Matrix/small
@@ -9,3 +9,7 @@ INPUT_MATRIX=$srcdir/Matrix/small
 #########################################
 INPUT_MATRIX=$srcdir/Matrix/medium
 ./lu 2 < $INPUT_MATRIX
+
+#########################################
+INPUT_MATRIX=$srcdir/Matrix/singular
+./lu 3 < $INPUT_MATRIX
