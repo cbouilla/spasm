7a9b7afb47ee61625f9deb0f0c3e7b1f9d98c90c
Charles Bouillaguet <charles.bouillaguet@lifl.fr>
2014-12-08 18:03:46 +0100

    command-line options in rank tool

--- bench/rank.c
+++ bench/rank.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
+#include <getopt.h>
+
 #include "spasm.h"
 
 #ifdef SPASM_TIMING
@@ -7,16 +9,58 @@ extern int64 reach, scatter, data_shuffling;
 #endif
 
 
-int main() {
+int main(int argc, char **argv) {
   spasm_triplet *T;
   spasm *A, *U, *L;
   spasm_lu *LU;
-  int *p;
+  int r, n, m, *p, ch, prime, allow_transpose, sort_strategy, keep_L;
   double start_time, end_time;
 
-  T = spasm_load_sms(stdin, 42013);
-  printf("A : %d x %d with %d nnz (loaded modulo 42013)\n", T->n, T->m, T->nz);
-  if (T->n < T->m) {
+  prime = 42013;
+  sort_strategy = 1; // cheap pivots by default
+  allow_transpose = 1;
+  keep_L = 0;
+
+  /* options descriptor */
+  struct option longopts[6] = {
+    { "sort-rows",    no_argument,       NULL,          's' },
+    { "keep-rows", no_argument,       NULL,             'k' },
+    { "no-transpose", no_argument,       NULL,          'a' },
+    { "modulus",      required_argument, NULL,          'p' },
+    { "keep-L" ,      no_argument,       NULL,          'l' },
+    { NULL,           0,                 NULL,           0  }
+  };
+
+  while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+    switch (ch) {
+    case 'p':
+      prime = atoi(optarg);
+      break;
+    case 'k':
+      sort_strategy = 0;
+      break;
+    case 't':
+      break;
+    case 'a':
+      allow_transpose = 0;
+      break;
+    case 's':
+      sort_strategy = 2;
+      break;
+    case 'l':
+      keep_L = 1;
+      break;
+    default:
+      printf("Unknown option\n");
+      exit(1);
+    }
+  }
+  argc -= optind;
+  argv += optind;
+
+  T = spasm_load_sms(stdin, prime);
+  printf("A : %d x %d with %d nnz (density = %.3f %%) -- loaded modulo %d\n", T->n, T->m, T->nz, 100.0 * T->nz / (1.0 * T->n * T->m), prime);
+  if (allow_transpose && (T->n < T->m)) {
     printf("[rank] transposing matrix\n");
     spasm_triplet_transpose(T);
   }
@@ -25,22 +69,33 @@ int main() {
 
   start_time = spasm_wtime();
 
-#ifdef SPASM_SORT_ROWS
-  p = spasm_row_sort(A);
-#else
-  p = spasm_cheap_pivots(A);
-#endif
+  switch(sort_strategy) {
+  case 0:
+    p = NULL;
+    break;
+  case 1:
+    p = spasm_cheap_pivots(A);
+    break;
+  case 2:
+    p = spasm_row_sort(A);
+    break;
+  }
 
-  LU = spasm_LU(A, p, 1);
+  LU = spasm_LU(A, p, keep_L);
   end_time = spasm_wtime();
   printf("\n");
 
   U = LU->U;
-  L = LU->L;
+  r = U->n;
+  n = A->n;
+  m = A->m;
 
   printf("LU factorisation (+ sort took) %.2f s\n", end_time - start_time);
-  printf("U :  %d x %d with %d nnz\n", U->n, U->m, spasm_nnz(U));
-  printf("L :  %d x %d with %d nnz\n", L->n, L->m, spasm_nnz(L));
+  printf("U :  %d x %d with %d nnz (density = %.1f %%)\n", r, m, spasm_nnz(U), 100.0 * spasm_nnz(U) / (r*m - r*r/2));
+  if (LU->L != NULL) {
+    L = LU->L;
+    printf("L :  %d x %d with %d nnz (density =%.1f %%)\n", L->n, r, spasm_nnz(L), 100.0 * spasm_nnz(L) / (r*n - r*r/2));
+  }
 
 #ifdef SPASM_TIMING
   printf("----------------------------------------\n");
