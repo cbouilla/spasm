006eeca366c89e51e684ebd39d572be820944209
Charles Bouillaguet <charles.bouillaguet@lifl.fr>
2015-03-27 16:21:19 +0100

    autotools magic for linbox

---
+++ bench/linbox_rank_wm.cpp
@@ -0,0 +1,38 @@
+#include "linbox/algorithms/gauss.h"
+#include "linbox/solutions/rank.h"
+
+#include <givaro/modular.h>
+#include <iostream>
+
+#define STOR_T SparseMatrixFormat::SparseSeq
+
+using namespace LinBox;
+
+int main (int argc, char **argv)
+{
+    typedef Givaro::Modular<uint32_t> Field;
+    typedef SparseMatrix<Field, STOR_T> Blackbox;
+
+    /* Params */
+    static integer q = 42013U;
+    std::string matfile = "data/sms.matrix";
+
+    Field F(q);
+
+    MatrixStream<Field> ms(F, std::cin); // read matrix on standard input
+    Blackbox A(ms);
+    size_t m, n;
+    ms.getDimensions(m, n);
+
+    /* Elements retournés */
+    unsigned long rank;
+
+    Method::Blackbox MB;
+    LinBox::rank(rank, A, MB);
+
+    /* Output */
+    std::cout << "====== RESULT =====" << std::endl << std::endl;
+    std::cout << "Rank: " << rank << std::endl;
+    return 0;
+}
+
