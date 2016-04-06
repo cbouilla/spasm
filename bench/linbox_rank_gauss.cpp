1c6a657325229800b96d9bbe3f4fca058a9cff53
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2016-04-06 09:33:47 +0200

    Ajout d'un timer à linbox_rank_gauss

--- bench/linbox_rank_gauss.cpp
+++ bench/linbox_rank_gauss.cpp
@@ -4,15 +4,49 @@
 
 #include <givaro/modular.h>
 #include <iostream>
+#include <getopt.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <unistd.h>
+
 
 #define STOR_T SparseMatrixFormat::SparseSeq
 
 using namespace LinBox;
 
+jmp_buf Env;
+
+void alarm_handler(int dummy) {
+  longjmp(Env, 1);
+}
+
 int main (int argc, char **argv)
 {
     typedef Givaro::Modular<uint32_t> Field;
     typedef SparseMatrix<Field, STOR_T> Blackbox;
+    int ch;
+    int timer = -1;
+
+    /* options descriptor */
+    struct option longopts[2] = {
+      { "max-time",     required_argument, NULL,          't' },
+      { NULL,           0,                 NULL,           0  }
+    };
+
+    while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+      switch (ch) {
+      case 't':
+        timer = atoi(optarg);
+        break;
+      default:
+        printf("Unknown option\n");
+        exit(1);
+      }
+    }
+    argc -= optind;
+    argv += optind;
+
+
 
     /* Params */
     static integer q = 42013U;
@@ -31,11 +65,21 @@ int main (int argc, char **argv)
     Method::Elimination ME;
     Timer chrono;
     chrono.clear();
+
+    if (timer > 0) {
+        signal(SIGALRM, alarm_handler);
+        alarm(timer);
+
+        if (setjmp(Env) != 0) {
+          fprintf(stderr, "\nTimeout after %d seconds\n", timer);
+          exit(1);
+        }
+    }
+
     chrono.start();
-
     LinBox::rank(rank, A, ME);
-
     chrono.stop();
+
 
     /* Output */
     std::cout << "====== RESULT =====" << std::endl << std::endl;
