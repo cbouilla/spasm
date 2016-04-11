eec2e986ee7032a54f65bcc8814886f96ae61593
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2016-04-11 19:55:07 +0200

    bug in filtering, maybe

--- bench/schur.c
+++ bench/schur.c
@@ -5,48 +5,47 @@
 int main(){
   
   /* charge la matrice depuis l'entrée standard */
-  int prime = 42013, i;
+  int prime = 42013, i, n_cheap, n_filtered, free_nnz, min, max;
+  float avg;
   spasm_triplet * T = spasm_load_sms(stdin, prime);
   spasm * A = spasm_compress(T);
   spasm_triplet_free(T);
 
   /* find free pivots. */
-  int n_cheap;
   int *p = spasm_cheap_pivots(A, &n_cheap);
+  int *filtered = malloc(n_cheap * sizeof(int));
 
-  /* filter rows that are too dense */
-  int free_nnz = 0;
-  int min = A->m;
-  int max = 0;
+  /* collect stats */
+  free_nnz = 0;
+  min = A->m;
+  max = 0;
   for(i=0; i<n_cheap; i++) {
       free_nnz += spasm_row_weight(A, p[i]);
       min = spasm_min(min, spasm_row_weight(A, p[i]));
       max = spasm_max(max, spasm_row_weight(A, p[i]));
   }
-  float avg = 1.0 * free_nnz / n_cheap;
-
+  avg = 1.0 * free_nnz / n_cheap;
   fprintf(stderr, "[schur] free pivots NNZ (min/avg/max): %d / %.1f / %d\n", min, avg, max);
-  
-  for(i=0; i<n_cheap; i++) {
-    int inew = p[i];
 
-    if (spasm_row_weight(A, inew) > 3*avg) {
-      /* this row is too dense */
-      spasm_swap(p, inew, --n_cheap);
-      i--;
-    } 
+  /* filter rows that are too dense */
+  n_filtered = 0;
+  for(i=0; i<n_cheap; i++) {
+    if (spasm_row_weight(A, p[i]) <= 3*avg) {
+      filtered[n_filtered++] = p[i];
+    }
   }
   
-  fprintf(stderr, "[schur] %d free pivots after filtering\n", n_cheap);
-
+  fprintf(stderr, "[schur] %d free pivots after filtering\n", n_filtered);
+  free(p);
 
   /* compute schur complement */
-  spasm *S = spasm_schur(A, p, n_cheap);
+  spasm *S = spasm_schur(A, filtered, n_filtered);
 
   fprintf(stderr, "Schur complement: (%d x %d), nnz : %d, dens : %.5f\n", S->n, S->m, spasm_nnz(S), 1. * spasm_nnz(S)/(1.*S->n * S->m));
 
-  spasm_save_csr(stdout, S);
-
-  spasm_csr_free(S);
+  free(filtered);
   spasm_csr_free(A);
+
+  spasm_save_csr(stdout, S);
+  spasm_csr_free(S);
 }
