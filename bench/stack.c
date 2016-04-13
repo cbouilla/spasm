ac4353a55900d42d386a6dd0c7ce1cd6e00973b4
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2016-04-13 10:38:44 +0200

    stacking matrices (removes empty rows / cols)

--- bench/stack.c
+++ bench/stack.c
@@ -7,21 +7,19 @@
     the permutation that reproduces the original.
  */
 
-int main(int argc, char **argv) {
-  spasm_triplet *T;
-  spasm *A;
-  int n, m, i, j, t, u, v;
-  int *p, *q, *Ap, *Aj;
+int main() {
+  spasm_triplet *A;
+  int n, m, nz, i, j, k, u, v;
+  int *p, *q, *Ai, *Aj;
 
-  T = spasm_load_sms(stdin, 42013);
-  A = spasm_compress(T);
-  spasm_triplet_free(T);
-
+  A = spasm_load_sms(stdin, 42013);
+  
   n = A->n;
   m = A->m;
-  Ap = A->p;
+  nz = A->nz;
+  Ai = A->i;
   Aj = A->j;
-
+  
   /* allocate result */
   p = spasm_malloc(n * sizeof(int));
   q = spasm_malloc(m * sizeof(int));
@@ -33,27 +31,39 @@ int main(int argc, char **argv) {
   }
 
   /* mark non-empty rows/columns */
-  for (i = 0; i < n; i++) {
-    for(t = Ap[i]; t < Ap[i + 1]; t++) {
-      j = Aj[t];
-      p[i] = 1;
-      q[j] = 1;
+  for (k = 0; k < nz; k++) {
+      p[ Ai[k] ] = 1;
+      q[ Aj[k] ] = 1;
+  }
+
+  /* sum-prefix: p[i] = sum(p[k], k in range(i)) */
+  u = 0;
+  for(i = 0; i<n; i++) {
+    if (p[i] > 0) {
+      p[i] = u++;
     }
   }
 
-  /* count non-empty rows/columns */
-  u = 0;
-  for(i = 0; i<n; i++) {
-    u += p[i];
-  }
   v = 0;
   for(j = 0; j<m; j++) {
-    v += q[j];
+    if (q[j] > 0) {
+      q[j] = v++;
+    }
   }
   
-  printf("matrix has advertized dimension %d x %d but is in fact %d x %d\n", n, m, u, v);
+  fprintf(stderr, "matrix has advertized dimension %d x %d but is in fact %d x %d\n", n, m, u, v);
   
-  spasm_csr_free(A);
+  /* modify matrix */
+  A->n = u;
+  A->m = v;
+  for (k = 0; k < nz; k++) {
+      Ai[k] = p[ Ai[k] ];
+      Aj[k] = q[ Aj[k] ];
+  }
+
+
+  spasm_save_triplet(stdout, A);
+  spasm_triplet_free(A);
   free(p);
   free(q);
   return 0;
