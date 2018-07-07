bcc209365322a8f34eb77d3a8d352b40d4c8c52b
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2018-07-07 10:18:21 +0200

    Fixes/improvements to v-modular partition

--- bench/tamu/process.py
+++ bench/tamu/process.py
@@ -38,23 +38,36 @@ def chain_decomposition(mtx_file):
     except subprocess.CalledProcessError as e:
         return -42, -42, -42
 
+
+
+def complement_cc(mtx_file):
+    """
+    Determine #cut vertex and #bridges.
+    """
+    cmd = "cat {} |  ~/spasm/build/bench/complement_cc".format(mtx_file)
+    try:
+        result = subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL)
+        return int(result.decode())
+    except subprocess.CalledProcessError as e:
+    #except ValueError as e:
+        return -42
+
+
 def modular_decomposition(mtx_file):
     """
     Determine #trivial modules, #nontrivial modules and average size of non-trivial modules
     """
     cmd = "cat {} |  ~/spasm/build/bench/modules".format(mtx_file)
-    try:
-        result = subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL)
-        cc, trivial, nontrivial, size, largest = map(int, result.decode().split(";"))
-        if nontrivial > 0:
-            size /= nontrivial
-        return cc, trivial, nontrivial, size, largest
-    except subprocess.CalledProcessError as e:
-            return -42, -42, -42, -42, -42
+    result = subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL)
+    n, nnz, trivial, nontrivial, size, largest, module_edges, quotient_edges = map(int, result.decode().split(";"))
+    if nontrivial > 0:
+        size /= nontrivial
+    return n, nnz, trivial, nontrivial, size, largest, module_edges, quotient_edges
+    #except subprocess.CalledProcessError as e:
+            #return -42, -42, -42, -42, -42, -42
+            #raise Runt
 
-
-
-print("id; name; year; size; nnz; kind; CC; link; cut; bridge; CCbis; trivial; nontrivial; size; largest")
+print("id, year, kind, link, trivial, nontrivial, size, largest, module_edges, quotient_edges")
 for x in tree.getroot():
     id = x.find("td[@class='column-id']").text
     name = x.find("td[@class='column-name']").text
@@ -71,10 +84,14 @@ for x in tree.getroot():
 
     try:
         mtx_file = ensure_mtx_file(link)
-        cc, cut, bridge = chain_decomposition(mtx_file)
-        cc_bis, trivial, nontrivial, size, largest = modular_decomposition(mtx_file)
+        #cc, cut, bridge = chain_decomposition(mtx_file)
+        #cc, cut, bridge = [-1, -1, -1]
+        n, nnz, trivial, nontrivial, size, largest, module_edges, quotient_edges = modular_decomposition(mtx_file)
+        #cc_bis = -1
         #cc_bis, trivial, nontrivial, size, largest = [-1, -1, -1, -1, -1]
-        print("{}; {}; {}; {}; {}; {}; {}; {}; {}; {}; {}; {}; {}; {:.1f}; {}"
-             .format(id, name, year, nrows, nnz, kind, link, cc, cut, bridge, cc_bis, trivial, nontrivial, size, largest))
-    except ValueError:
+        print("{}; {}; {}; {}; {}; {}; {}; {}; {:.1f}; {}; {}; {}"
+             .format(id, n, nnz, year, kind, link, trivial, nontrivial, size, largest, module_edges, quotient_edges))
+        # ccc = complement_cc(mtx_file)
+        #print("{}; {}; {}; {}; {}; {}; {}".format(id, year, nrows, nnz, kind, link, ccc))
+    except subprocess.CalledProcessError:
         pass
